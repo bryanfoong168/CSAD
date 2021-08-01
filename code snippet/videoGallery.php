c96c6fbf0ce0eed03c42c53b4016389e6b34cc7e
bryanfoong168 <56813783+bryanfoong168@users.noreply.github.com>
2021-08-01 23:54:00 +0800

    Update videoGallery.php

--- code snippet/videoGallery.php
+++ code snippet/videoGallery.php
@@ -12,9 +12,48 @@ and open the template in the editor.
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css" />
     <script src="https://code.jquery.com/jquery-1.11.3.js"></script>
-<script>
 
-</script>
+    <style>
+        .rate {
+    position: inherit;
+            margin-left: auto;
+            margin-right: auto;
+    float: left;
+    height: 46px;
+    padding: 0 10px;
+}
+.rate:not(:checked) > input {
+    position:absolute;
+    top:-9999px;
+}
+.rate:not(:checked) > label {
+    float:right;
+    width:1em;
+    overflow:hidden;
+    white-space:nowrap;
+    cursor:pointer;
+    font-size:30px;
+    color:#ccc;
+}
+.rate:not(:checked) > label:before {
+    content: '★ ';
+}
+.rate > input:checked ~ label {
+    color: #ffc700;    
+}
+.rate:not(:checked) > label:hover,
+.rate:not(:checked) > label:hover ~ label {
+    color: #deb217;  
+}
+.rate > input:checked + label:hover,
+.rate > input:checked + label:hover ~ label,
+.rate > input:checked ~ label:hover,
+.rate > input:checked ~ label:hover ~ label,
+.rate > label:hover ~ input:checked ~ label {
+    color: #c59b08;
+}
+    </style>
+    
     <body>
         <?php
 $arr_video_ids = array(
@@ -52,12 +91,22 @@ function extractVideoID($url){
             ?>
             <div class="col-md-4">
                 <div class="pb-2">
-                    <form id="myform" name="myform" method="get" action="course.php">
-                        <a href="javascript:void()" onclick="submitform()">
-                            <img src="<?php echo $video_thumbnail; ?>" class="img-thumbnail" />
-                        </a>
+                    <form action="course.php" method="get">
+                        <input style="display:none;" type="text" name="name" value="<?php echo $video_id ?>">
+                        <input type="image" src="<?php echo $video_thumbnail; ?>" class="img-thumbnail" alt="Submit" name="name">
                     </form>
-                    
+                    <div class="rate">
+                        <input type="radio" id="star5" name="rate" value="5" />
+                        <label for="star5" title="text">5 stars</label>
+                        <input type="radio" id="star4" name="rate" value="4" />
+                        <label for="star4" title="text">4 stars</label>
+                        <input type="radio" id="star3" name="rate" value="3" />
+                        <label for="star3" title="text">3 stars</label>
+                        <input type="radio" id="star2" name="rate" value="2" />
+                        <label for="star2" title="text">2 stars</label>
+                        <input type="radio" id="star1" name="rate" value="1" />
+                        <label for="star1" title="text">1 star</label>
+                      </div>
                 </div>
             </div>
         
@@ -65,10 +114,7 @@ function extractVideoID($url){
     </div>
 </div>
         <script>
-            function submitform() {   
-                var video_id = 50;
-                document.getElementById('myform').submit();
-            }
+            
         </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
