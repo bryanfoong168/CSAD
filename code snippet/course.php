7126c9215f132fda5defe7c3581687094363515a
bryanfoong168 <56813783+bryanfoong168@users.noreply.github.com>
2021-08-01 14:09:22 +0800

    Update course.php

--- code snippet/course.php
+++ code snippet/course.php
@@ -10,14 +10,20 @@ and open the template in the editor.
         <title></title>
     </head>
     <style>
-        .col-md-4{
+        .videoContainer{
+            display: flex;
             margin: auto;
+        }
+        #player{
+            position: inherit;
+            margin-left: auto;
+            margin-right: auto;
         }
     </style>
     <body>
         <?php
 $arr_video_ids = array(
-    'https://www.youtube.com/watch?v=Pzv_lUp3iOQ',
+    'https://www.youtube.com/embed/tgbNymZ7vqY',
 );
  
 function getYouTubeThumbnailImage($video_id) {
@@ -38,13 +44,43 @@ function extractVideoID($url){
             $video_id = extractVideoID($video);
             $video_thumbnail = getYouTubeThumbnailImage($video_id);
             ?>
-            <div class="col-md-4">
-                <div class="pb-2">
-                    <a class="videos" data-fancybox="video-gallery" href="<?php echo $video; ?>">
-                        <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
-                    </a>
-                </div>
+            <div class="videoContainer">
+                <div id="player"></div>
+<script src="http://www.youtube.com/player_api"></script>
+<script>
+    
+    // create youtube player
+    var player;
+    function onYouTubePlayerAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: '<?php echo $video_id?>', <!-- video id should be here. last component from video URL -->
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+    }
+
+    // autoplay video
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
+
+    // when video ends
+    function onPlayerStateChange(event) {        
+        if(event.data === 0) {            
+            doSomeAction();
+        }
+    }
+    function doSomeAction(){
+    	alert("Hello! I am an alert box!");
+    }
+    
+</script>
             </div>
+            
         <?php } ?>
     
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
