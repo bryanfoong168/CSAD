4afb413f1d4aa8ec1dc3f0ea380c14ff54e31ac1
bryanfoong168 <56813783+bryanfoong168@users.noreply.github.com>
2021-08-01 23:54:25 +0800

    Update course.php

--- code snippet/course.php
+++ code snippet/course.php
@@ -8,6 +8,9 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" type="text/css" href="starStyle.css.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css" />
     </head>
     <style>
         .videoContainer{
@@ -20,8 +23,55 @@ and open the template in the editor.
             margin-right: auto;
         }
     </style>
+    
+    <style>
+        .starContainer{
+            display: flex;
+            margin: auto;
+        }
+.rate {
+    position: inherit;
+            margin-left: auto;
+            margin-right: auto;
+    float: left;
+    height: 46px;
+    padding: 0 10px;
+}
+.rate:not(:checked) > input {
+    position:absolute;
+    top:-9999px;
+}
+.rate:not(:checked) > label {
+    float:right;
+    width:1em;
+    overflow:hidden;
+    white-space:nowrap;
+    cursor:pointer;
+    font-size:30px;
+    color:#ccc;
+}
+.rate:not(:checked) > label:before {
+    content: '★ ';
+}
+.rate > input:checked ~ label {
+    color: #ffc700;    
+}
+.rate:not(:checked) > label:hover,
+.rate:not(:checked) > label:hover ~ label {
+    color: #deb217;  
+}
+.rate > input:checked + label:hover,
+.rate > input:checked + label:hover ~ label,
+.rate > input:checked ~ label:hover,
+.rate > input:checked ~ label:hover ~ label,
+.rate > label:hover ~ input:checked ~ label {
+    color: #c59b08;
+}
+    </style>
     <body>
         <?php
+        $video_id = $_GET["name"];
+        
 $arr_video_ids = array(
     'https://www.youtube.com/embed/tgbNymZ7vqY',
 );
@@ -36,50 +86,68 @@ function extractVideoID($url){
     return $video[7];
 }
 ?>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css" />
+        
+        
+
 
         <?php foreach ($arr_video_ids as $video) { ?>
             <?php
-            $video_id = extractVideoID($video);
+            #$video_id = extractVideoID($video);
             $video_thumbnail = getYouTubeThumbnailImage($video_id);
             ?>
+        
             <div class="videoContainer">
-                <div id="player"></div>
-<script src="http://www.youtube.com/player_api"></script>
-<script>
-    
-    // create youtube player
-    var player;
-    function onYouTubePlayerAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: '<?php echo $video_id?>', <!-- video id should be here. last component from video URL -->
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-    }
+                <div id="player">
+                    
+                </div>
+                    <script src="http://www.youtube.com/player_api"></script>
+                    <script>
+                        // create youtube player
+                        var player;
+                        function onYouTubePlayerAPIReady() {
+                            player = new YT.Player('player', {
+                              height: '480',
+                              width: '720',
+                              videoId: '<?php echo $video_id?>', <!-- video id should be here. last component from video URL -->
+                              events: {
+                                'onReady': onPlayerReady,
+                                'onStateChange': onPlayerStateChange
+                              }
+                            });
+                        }
 
-    // autoplay video
-    function onPlayerReady(event) {
-        event.target.playVideo();
-    }
+                        // autoplay video
+                        function onPlayerReady(event) {
+                            event.target.playVideo();
+                        }
 
-    // when video ends
-    function onPlayerStateChange(event) {        
-        if(event.data === 0) {            
-            doSomeAction();
-        }
-    }
-    function doSomeAction(){
-    	alert("Hello! I am an alert box!");
-    }
-    
-</script>
+                        // when video ends
+                        function onPlayerStateChange(event) {        
+                            if(event.data === 0) {            
+                                doSomeAction();
+                            }
+                        }
+                        function doSomeAction(){
+                            alert("Video has ended");
+                        }
+                    </script>
+                        <br>
+                    
             </div>
+        <div class="starContainer">
+                        <div class="rate">
+                        <input type="radio" id="star5" name="rate" value="5" />
+                        <label for="star5" title="text">5 stars</label>
+                        <input type="radio" id="star4" name="rate" value="4" />
+                        <label for="star4" title="text">4 stars</label>
+                        <input type="radio" id="star3" name="rate" value="3" />
+                        <label for="star3" title="text">3 stars</label>
+                        <input type="radio" id="star2" name="rate" value="2" />
+                        <label for="star2" title="text">2 stars</label>
+                        <input type="radio" id="star1" name="rate" value="1" />
+                        <label for="star1" title="text">1 star</label>
+                  </div>
+</div>
             
         <?php } ?>
     
